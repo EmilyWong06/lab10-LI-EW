f431b2e1c97b9df4b711b30c53531e9beab383ff
LiaIssakov <issakovl@ufl.edu>
2025-04-14 14:58:31 -0400

    merged changes in test file

--- test_calculator.py
+++ test_calculator.py
@@ -3,12 +3,15 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(4, 5), 9)
+        self.assertEqual(add(10, 0), 10)
+        self.assertEqual(add(0, 10), 10)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(subtract(5, 5), 0)
+        self.assertEqual(subtract(5, 10), -5)
+        self.assertEqual(subtract(6, 4), 2)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +22,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertAlmostEqual(logarithm(8, 2), 3)
+        self.assertAlmostEqual(logarithm(1, 5), 0)
+        self.assertAlmostEqual(logarithm(27, 3), 3)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, 10)
+        with self.assertRaises(ValueError):
+            logarithm(10, 0)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
